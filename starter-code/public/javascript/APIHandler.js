4f07a3a373af017eeafd4ab861eddbdfde3e5cd3
Sany Chernizon <chernizon@gmail.com>
2019-04-27 18:04:37 -0300

    feito fetch all e fetch one

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,62 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  renderPerson(name, occupation, cartoon, weapon) {
+  
+    var divName = document.createElement('div');
+    divName.className = 'name';
+    divName.innerHTML = name;
+    var divOccupation = document.createElement('div');
+    divOccupation.className = 'occupation';
+    divOccupation.innerHTML = occupation;
+    var divCartoon = document.createElement('div');
+    divCartoon.className = 'cartoon';
+    cartoon ? divCartoon.innerHTML = 'Sim!' : divCartoon.innerHTML = 'Não!'
+    var divWeapon = document.createElement('div');
+    divWeapon.className = 'weapon';
+    divWeapon.innerHTML = weapon;
+  
+    var divCharacterInfo = document.createElement('div');
+    divCharacterInfo.className = 'character-info';
+    divCharacterInfo.appendChild(divName);
+    divCharacterInfo.appendChild(divOccupation);
+    divCharacterInfo.appendChild(divCartoon);
+    divCharacterInfo.appendChild(divWeapon);
+  
+    var divCharactersContainer = document.querySelector('.characters-container');
+    divCharactersContainer.appendChild(divCharacterInfo);
   }
 
-  getOneRegister () {
+  getFullList(person) {
+    var divCharactersContainer = document.querySelector('.characters-container');
+    divCharactersContainer.innerHTML = '';
 
+    person.forEach((item) => {
+      let {name,occupation,weapon,cartoon} = item;
+      this.renderPerson(name,occupation,weapon,cartoon);
+    })
   }
 
-  createOneRegister () {
+  getOneRegister(person) {
+    var divCharactersContainer = document.querySelector('.characters-container');
+    divCharactersContainer.innerHTML = '';
 
+    console.log(person);
+    let {name,occupation,weapon,cartoon} = person;
+    this.renderPerson(name,occupation,weapon,cartoon);
   }
 
-  updateOneRegister () {
-
+  createOneRegister() {
+    console.log('criou')
   }
 
-  deleteOneRegister () {
+  updateOneRegister() {
+    console.log('updatou')
+  }
 
+  deleteOneRegister() {
+    console.log('deletou')
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,50 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    axios.get('http://localhost:8000/characters')
+      .then((person) => {
+        charactersAPI.getFullList(person.data)
+      }).catch((error) => {
+        console.log(error)
+      })
+  }
 
+  document.getElementById('fetch-one').onclick = function () {
+    var charId = document.querySelector('input[name=character-id]');
+    axios.get('http://localhost:8000/characters/' + charId.value)
+    .then((person) => {
+      charactersAPI.getOneRegister(person.data)
+    }).catch((error) => {
+      console.log(error)
+    })
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+    var charId = document.querySelector('input[name=character-id]');
+    charactersAPI.DeleteOneRegister()
+    axios.delete('http://localhost:8000/characters/:id');
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+
+  // document.getElementById('edit-character-form').onsubmit = function () {
+
+  //   axios.put('http://localhost:8000/characters/:id');
+  // }
+
+  // document.getElementById('new-character-form').onsubmit = function () {
+
+  //   axios.post('http://localhost:8000/characters/');
+  // }
+});
+
+// btnFetchAll = document.querySelector('#fetch-all');
+// btnFetchOne = document.querySelector('#fetch-one');
+// btnCreate = document.querySelector('#send-data');
+// btnDelete = document.querySelector('#delete-one');
+// btnUpdate = document.querySelector('#update-data');
+
+// btnFetchAll.addEventListener('click', charactersAPI.getFullList)
+// btnFetchOne.addEventListener('click', charactersAPI.getOneRegister)
+// btnCreate.addEventListener('click', charactersAPI.createOneRegister)
+// btnDelete.addEventListener('click', charactersAPI.deleteOneRegister)
+// btnUpdate.addEventListener('click', charactersAPI.updateOneRegister)
